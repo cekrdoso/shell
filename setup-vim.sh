65a69a0d79c978b7441c5a35f227a6535bf1e06c
Carlos Eduardo Cardoso <cekrdoso@gmail.com>
2019-04-20 15:35:16 -0300

    few changes to colorscheme

--- setup-vim.sh
+++ setup-vim.sh
@@ -5,5 +5,5 @@ cat vim/vimrc > ~/.vimrc
 rm -rf ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-echo 'colorscheme elise' >> ~/.vimrc
+echo 'colorscheme molokai_dark' >> ~/.vimrc
 cp -f vim/setcolors.vim ~/.vim/plugin/setcolors.vim
