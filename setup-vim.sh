ffbea68b3fdae00d4c0d4fafff36c362fdeb9719
Carlos Eduardo Cardoso <cekrdoso@gmail.com>
2019-04-21 00:05:15 -0300

    some changes

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 mkdir -p ~/.vim/plugin
+mkdir -p ~/.vim/bundle
 cat vim/vimrc > ~/.vimrc
 rm -rf ~/.vim/bundle/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git \
+    ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-echo 'colorscheme molokai_dark' >> ~/.vimrc
+echo -e \
+    "\n\" Colorscheme\ncolorscheme molakai_dark" >> ~/.vimrc
 cp -f vim/setcolors.vim ~/.vim/plugin/setcolors.vim
