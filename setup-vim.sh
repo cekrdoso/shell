95d7bf686519881c298d08e8bec9f4cabda30869
cekrdoso <37446460+cekrdoso@users.noreply.github.com>
2019-04-22 22:33:31 -0300

    Update setup-vim.sh

--- setup-vim.sh
+++ setup-vim.sh
@@ -8,5 +8,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git \
     ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 echo -e \
-    "\n\" Colorscheme\ncolorscheme molakai_dark" >> ~/.vimrc
+    "\n\" Colorscheme\ncolorscheme molokai_dark" >> ~/.vimrc
 cp -f vim/setcolors.vim ~/.vim/plugin/setcolors.vim
